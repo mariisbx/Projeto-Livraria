ba8618fb715cbc9f5d23732ec930b3699ee9088a
mariisbx <mariaaparecida.ifc.araquari@gmail.com>
2025-05-10 21:18:17 -0300

    FEAT: Criação e formatação do carrinho #5

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,49 @@
   }
 
   let livroSelecionado = ref(sorteando());
+  let carrinho = ref([]);
+  function adicionarCarrinho(livro) {
+  const existente = carrinho.value.find(item => item.id === livro.id);
+
+  if (existente) {
+    existente.quantidade++;
+  } else {
+    livro.adicionado = true;
+    livro.quantidade = 1;
+    carrinho.value.push(livro);
+  }
+}
+  const carrinhoVisivel = ref(false);
+  const lojaVisivel = ref(true);
+
+  function mostrarCarrinho() {
+  carrinhoVisivel.value = true;
+  lojaVisivel.value = false;
+}
+
+  function mostrarLoja() {
+  carrinhoVisivel.value = false;
+  lojaVisivel.value = true;
+}
+
+  function incrementar(item){
+    item.quantidade++;
+  }
+  function decrementar(item){
+    if (item.quantidade > 1) {
+    item.quantidade--;
+  }
+}
+
+  function removerItem(item) {
+    const lixeira = carrinho.value.findIndex(livro => livro.id === item.id);
+    if (lixeira !== -1) {
+      carrinho.value.splice(lixeira, 1);
+      item.adicionado = false;
+      item.quantidade = 0;
+      }
+    }
+
 </script>
 
 <template>
@@ -117,7 +160,62 @@
         </ul>
       </section>
     </main>
+
     </body>
+    </div>
+<div v-else>
+  <section class="carrinho" v-if="carrinho.length > 0">
+      <h2>Seu Carrinho</h2>
+      <div class="divcarrinho">
+      <div>
+      <ul v-for="item in carrinho" :key="item.id">
+        <li>
+          <p>
+          <img :src="item.capa" alt="livro" id="capaCarrinho">
+          </p>
+        </li>
+          <li id="espaco" >
+            {{ item.nome }} - R${{ (parseFloat(item.preco.replace('R$', '').replace(',', '.')) * item.quantidade).toFixed(2) }}
+          </li>
+          <li id="espaco">
+            <button @click="incrementar(item)">+</button>
+          </li>
+          <li id="espaco">
+             {{ item.quantidade}}
+          </li>
+         <li id="espaco">
+          <button @click="decrementar(item)">-</button>
+         </li>
+          <li id="espaco">
+  <button @click="removerItem(item)"><span class="fa-solid fa-trash"></span></button>
+          </li>
+      </ul>
+    </div>
+    <div class="finalcompra">
+     <h3 class="bordapreco">Total</h3>
+     <div class="separarP">
+      <p>Quantidade de itens:</p>
+      <p class="bordamargin">{{ carrinho.reduce((total, item) => total + item.quantidade, 0)}}</p>
+     </div>
+     <div class="separarFrete">
+     <p>Frete:</p>
+     <p class="bordaespaco">Grátis</p>
+    </div>
+      <p class="pagamento"> Pagamento: R$ {{ carrinho.reduce((soma, item) => {
+        const preco = parseFloat(item.preco.replace('R$', '').replace(',', '.'));
+        return soma + (preco * item.quantidade);
+      }, 0).toFixed(2) }}</p>
+    </div>
+  </div>
+      <button class="voltarloja" @click="mostrarLoja">Voltar à loja</button>
+    </section>
+    <section class="carrinhovazio" v-else>
+      <span class="fa-solid fa-cart-plus"></span>
+      <p>Seu carrinho está vazio</p>
+      <p>Clique aqui para adicionar itens:</p>
+      <button @click="mostrarLoja">Loja</button>
+    </section>
+  </div>
 </template>
 
 <style scoped>
@@ -226,42 +324,6 @@ img {
 .exibicao div.texto p.sinapse {
   font-size: 1.2rem;
 }
-.loja ul {
-  display: flex;
-  flex-wrap: wrap;
-  margin: 0 3vw 0 7.5vw ;
-  justify-content: center;
-  padding: 0;
-}
-.loja ul li {
-  margin: 6vw 1.5vw 3vw 1.5vw;
-  width: 320px;
-  margin: 2vw;
-}
-.loja h3 {
-  font-family: "Patua One", serif;
-  margin: 20px 0 0 0 ;
-  font-size: 1.2rem;
-}
-.loja p {
-  font-size: 1.1rem;
-  margin: 10px 0 10px 0;
-}
-.loja button {
-  font-size: 1.2rem;
-  padding: 10px 100px 10px 100px;
-  color: #FF5722;
-  background-color: white;
-  border: 2px solid #FF5722;
-  margin: 10px 0 0 0;
-  margin: 1vw 0 0 3px;
-}
-.loja h2 {
-  font-size: 1.8rem;
-  margin: 5vw 6vw 3vw 6vw;
-  border-bottom: 2px solid #FF5722;
-  width: 14%;
-  padding: 0 0 10px 0;
 .categorias {
   margin: 4vw 0 0 0;
   border-bottom: 2px solid #FF5722;
@@ -298,5 +360,148 @@ img {
 .categorias ul li.borderb {
   border-left: #FF5722 solid 3px;
   padding: 0 7vw;
+}
+.loja ul {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 3vw 0 7.5vw ;
+  justify-content: center;
+  padding: 0;
+}
+.loja ul li {
+  margin: 6vw 1.5vw 3vw 1.5vw;
+  width: 320px;
+  margin: 2vw;
+}
+.loja h3 {
+  font-family: "Patua One", serif;
+  margin: 20px 0 0 0 ;
+  font-size: 1.2rem;
+}
+.loja p {
+  font-size: 1.1rem;
+  margin: 10px 0 10px 0;
+}
+.loja button {
+  font-size: 1.2rem;
+  padding: 10px 100px 10px 100px;
+  color: #FF5722;
+  background-color: white;
+  border: 2px solid #FF5722;
+  margin: 10px 0 0 0;
+  margin: 1vw 0 0 3px;
+}
+.loja h2 {
+  font-size: 1.8rem;
+  margin: 5vw 6vw 3vw 6vw;
+  border-bottom: 2px solid #FF5722;
+  width: 14%;
+  padding: 0 0 10px 0;
+}
+.carrinho div.divcarrinho {
+  display: flex;
+}
+.carrinho img#capaCarrinho{
+  width: 230px;
+  height: 300px;
+  margin: 0 28px 0 0;
+}
+.carrinho h2{
+  margin: 3vw 3vw 0 3vw;
+  font-size: 2rem;
+  font-weight: 700;
+  border-bottom: 2px solid #FF5722 ;
+  width: 11%;
+  padding: 0 0 8px 0;
+}
+.carrinho ul {
+  display: flex;
+  align-items: center;
+  border-bottom: #FF5722 solid 2px;
+  margin: 0 0 0 3vw;
+  padding: 0 0 30px 0;
+}
+.carrinho ul li {
+  margin: 4vw 1.2vw 2vw 5vw;
+}
+.carrinho ul li#espaco {
+  margin: 2vw 10px 0 0 ;
+  font-size: 1.3rem;
+}
+.carrinho li#espaco button {
+  font-size: 1.4rem;
+  padding: 4px 8px;
+  background-color: white;
+  border: none;
+  border-radius: 5px;
+  color: #7B7881;
+}
+.carrinho div.finalcompra {
+  margin: 6vw 0 0 17vw;
+  border: 2px solid #FF5722;
+  padding: 3vw 3vw 0 3vw;
+  height: 14vw;
+}
+.carrinho div.separarP {
+  display: flex ;
+  border-bottom: 2px solid #FF5722;
+  padding: 0 0 14px 0;
+}
+.carrinho div.separarFrete {
+  display: flex;
+  border-bottom: 2px solid #FF5722;
+  padding: 0 0 14px 0;
+}
+.carrinho h3{
+  font-size: 1.6rem
+}
+.carrinho p{
+  font-size: 1.2rem;
+  margin: 1vw 0 0 0;
+}
+.carrinho div.finalcompra p.bordapreco{
+  border-bottom: 2px solid #FF5722;
+  padding: 0 0 14px 0;
+}
+.carrinho div.finalcompra p.bordamargin {
+  margin: 19px 0 0 30px;
+}
+.carrinho div.finalcompra h3.bordapreco {
+  border-bottom: 2px solid #FF5722;
+  padding: 0 0 14px 0;
+}
+.carrinho div.finalcompra p.bordaespaco {
+  margin: 19px 0 0 130px;
+}
+.carrinho p.pagamento {
+  text-align: center;
+}
+.carrinho button.voltarloja {
+  font-size: 1.2rem;
+  padding: 6px;
+  margin: 2vw 5vw;
+  background-color: white;
+  border: 2px solid #FF5722;
+  border-radius: 10px;
+}
+.carrinhovazio {
+  text-align: center;
+  margin: 17vw 0 0 0;
+}
+.carrinhovazio span {
+  color: #FF5722;
+  font-size: 5rem;
+}
+.carrinhovazio p {
+  font-size: 1.6rem;
+  margin: 10px;
+}
+.carrinhovazio button {
+  font-size: 1.4rem;
+  padding: 6px;
+  color: #FF5722;
+  border: 2px solid #FF5722;
+  border-radius: 5px;
+  margin: 10px;
 }
 </style>
